fa9eb268b5ab06bc6c3de2ee58a3658af0246b32
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2023-03-08 19:20:14 +0530

    Minor Sidebar update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Stack } from '@mui/material';
+import { categories } from '../utils/Constant';
+
+
+
+const selectedCategory = "New";
 
 const Sidebar = () => {
+
+
   return (
-    <div>Sidebar</div>
+    <>
+        <Stack direction="row" sx={{overflowY:"auto", height:{sx:"auto", md:"95%"}, flexDirection:{md:'column'}}}>
+            {categories.map((category)=>(
+                <button key={category.name} className="category-btn" style={{background: category.name === selectedCategory && "#FC1503", color:"white"}}>
+                    <span>{category.icon}</span>
+                    <span>{category.name}</span>
+                </button>
+                )
+            )}
+        </Stack>
+    </>
   )
 }
 
