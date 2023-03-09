a3c3671848ba7ac281706472ea8e7f07b6a9b0d8
harshgupta20 <hgreatgupta2001@gmail.com>
2023-03-10 00:27:12 +0530

    channel page ready with title

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/Constant';
 import ChannelDetail from './ChannelDetail';
 
-const ChannelCard = ({channelDetail}) => {
+const ChannelCard = ({channelDetail, marginTop}) => {
   return ( 
     <>
-        <Box sx={{boxShadow:'none', borderRadius:'20px', display:'flex', justifyContent:'center', alignItems:'center', width:{xs:'356px', md:'320px'}, height:'326px', margin:'auto'}}>
+        <Box sx={{boxShadow:'none', borderRadius:'20px', display:'flex', justifyContent:'center', alignItems:'center', width:{xs:'356px', md:'320px'}, height:'326px', margin:'auto', marginTop:{marginTop}}}>
             <Link to={`/channel/${channelDetail?.id?.channelId}`}>
             <CardContent sx={{display:'flex', flexDirection:'column', justifyContent:'center', textAlign:'center', color:'#fff'}}>
                 <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} alt={channelDetail?.snippet?.title} sx={{borderRadius:'50px', height:'180px', width:'180px', mb:2, border:'1px solid #e3e3e3'}}/>
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -3,11 +3,33 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { Box } from '@mui/material';
+import { fetchFromAPI } from '../utils/FetchFromAPI';
+import ChannelCard from './ChannelCard';
 
 const ChannelDetail = () => {
   const {id} = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState();
+  
+  // console.log(channelDetail);
+  // console.log(videos);
+
+  useEffect(()=>{
+    // to get the channel info
+    fetchFromAPI(`channels?part="snippet&id=${id}`).then((data)=> setChannelDetail(data?.items[0]));
+    // to get the video of particular channel
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data)=> setVideos(data?.items));
+  }, [id]);
+
   return (
     <> 
+
+    <Box minHeight="95vh">
+        <Box>
+          <div style={{background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)', zIndex:10, height:'300px'}} />
+          <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+        </Box>
+    </Box>
       {id}
     </>
   )
