b9daf64b2a94a9bb5fe2ab74cc198ec1a7f72370
harshgupta20 <hgreatgupta2001@gmail.com>
2023-03-10 00:46:19 +0530

    Channel page completed

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -5,11 +5,12 @@ import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { fetchFromAPI } from '../utils/FetchFromAPI';
 import ChannelCard from './ChannelCard';
+import Video from './Video';
 
 const ChannelDetail = () => {
   const {id} = useParams();
   const [channelDetail, setChannelDetail] = useState(null);
-  const [videos, setVideos] = useState();
+  const [videos, setVideos] = useState(null);
   
   // console.log(channelDetail);
   // console.log(videos);
@@ -28,6 +29,12 @@ const ChannelDetail = () => {
         <Box>
           <div style={{background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)', zIndex:10, height:'300px'}} />
           <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+        </Box>
+
+        <Box display='flex' p='2'>
+          <Box sx={{mr: { sm:'100px'}}} />
+            {videos ? <Video videos={videos} /> : ""}
+              {/* <Video videos={videos} /> */}
         </Box>
     </Box>
       {id}
