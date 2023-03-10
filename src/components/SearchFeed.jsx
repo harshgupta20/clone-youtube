b673cb026122bb533c09431897fbb235aacfa2dc
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2023-03-10 23:02:43 +0530

    searhfeed code ready

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box, Typography } from '@mui/material';
+import Video from './Video';
+
+import { fetchFromAPI } from '../utils/FetchFromAPI';
 
 const SearchFeed = () => {
+
+  const [videos, setVideos] = useState([]);
+  const [SearchTerm] = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${SearchTerm}`).then((data) => setVideos(data.items));
+  }, [SearchTerm])
+
+  console.log("start");
+  console.log(videos);
+  console.log("end");
   return (
-    <div>SearchFeed</div>
+    <>
+        {/* Feed Body */}
+        <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+          {/* Feed Title */}
+          <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white' }}>Search Videos for :  <span style={{ color: "#F31503" }}>{SearchTerm}</span> Video</Typography>
+          {/* Video Content of Feed */}
+          <Video videos={videos} />
+        </Box>
+
+    </>
   )
 }
 
-export default SearchFeed
+export default SearchFeed;
